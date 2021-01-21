20600726ea44c38f7790acaffd1aa7105eaad348
fyin-stats <yinf2@uci.edu>
2021-01-21 01:36:38 -0800

    initial

---
+++ chapter1.py
@@ -0,0 +1,37 @@
+################
+# https://learn.datacamp.com/courses/supervised-learning-with-scikit-learn
+# supervised learning with scikit learn
+# machine learning: the art and science of giving computers the ability to learn to make decisions from data
+# without being explicitly programmed
+
+# learning to predict spam emails
+# clustering wikipedia entries into different categories
+
+# Unsupervised learning: uncovering hidden patterns from unlabeled data
+
+# Reinforcement learning: software agents interact with an environment
+# learn how to optimize their behavior
+# given a system of rewards and punishments
+# draws inspiration from behavioral psychology
+#
+
+# Applications:
+# economics
+# genetics
+# game playing
+
+# Supervised learning
+# predictor variables/features and a target variable
+# Aim: Predict the target variable, given the predictor variables
+# naming conventions:
+# features = predictor variables = independent variables
+# target variable = response variable
+
+# automate time-consuming or expensive manual tasks
+# make predictions about the future
+# need labeled data -- historical data with labels, experiments to get labeled data, crowd sourcing labeled data
+#
+
+
+# scikit-learn / sklearn
+# Integrates well with the SciPy stack
